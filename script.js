8c7a34c1b7eb2a3bca50b4f05bafb39248a9f24d
sachin <sachinsain21082004@gmail.com>
2023-06-14 13:17:41 +0530

    js work

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
   let container = document.querySelector("#gridContainer");
-
   for (let i = 0; i < 81; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
-    
     container.appendChild(box);
   }
   let boxAll = document.querySelectorAll(".box");
   let button = document.querySelector("button");
-  let store= document.querySelector("#gameScore") 
-  let randomBox =[];
-  for(let j=0;j<9;j++){
-    let num =Math.floor(Math.random()*81)
+  let store = document.querySelector("#gameScore")
+  let randomBox = [];
+  let countScore = 0;
+  for (let j = 0; j < 9; j++) {
+    let num = Math.floor(Math.random() * 81)
     randomBox.push(num)
   }
-  console.log(boxAll);
- 
+  let check = false
   for (let i = 0; i < boxAll.length; i++) {
     boxAll[i].onclick = () => {
-      if(boxAll[i].classList.contains("box2")){
-       randomBox.forEach ((j)=>{
-        boxAll[j].style.display="block"
-       })
-        
+      if (boxAll[i].classList.contains("box2")) {
+        check = true;
+        if (check){
+          for (let j = 0; j < boxAll.length; j++) {
+            if (boxAll[j].hasAttribute("id")){
+              boxAll[j].removeAttribute("id")
+            }
+          }
+        }
       }
-      else{
-        boxAll[i].style.backgroundColor="green"
+      else {
+        if(boxAll[i].classList.contains("box1")){
+          return false
+        }
+        else{
+          countScore++
+          store.innerHTML=countScore
+          boxAll[i].classList.add("box1")
+        }
       }
-     
     };
-    randomBox.forEach(element => {
-      if(element===(i)){  
+    randomBox.forEach((element )=> {
+      if (element === (i)) {
         boxAll[i].classList.add("box2")
-        boxAll[i].style.background="url(./bom1.png) no-repeat scroll center / cover"
-        boxAll[i].style.backgroundColor="red"
+        boxAll[i].setAttribute("id", "boxId")
       }
     });
-    console.log(randomBox)
-    
-    console.log(boxAll[i]);
-      
   }
-     
 });
 
 
